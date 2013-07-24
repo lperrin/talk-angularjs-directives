56b1cb2faaa1c2ccb1ba9ffc150df35cdbf7468b
Laurent Perrin <laurent.perrin@gmail.com>
2013-07-24 15:59:54 +0200

    with ngIf.

--- 5_blockquote/blockquote.js
+++ 5_blockquote/blockquote.js
@@ -1,6 +1,6 @@
 var demoApp = angular.module('demoApp', []);
 
-demoApp.directive('blockquote', ['$animator', function ($animator) {
+demoApp.directive('blockquote', function ($animator) {
   return {
     replace: true,
 
@@ -10,33 +10,8 @@ demoApp.directive('blockquote', ['$animator', function ($animator) {
 
     templateUrl: 'blockquote-tpl',
 
-    compile: function (element, attrs, transclude) {
-      return function ($scope, $element, $attrs) {
-        var childElement, childScope;
-        var animate = $animator($scope, $attrs);
-
-        $scope.hidden = true;
-
-        $scope.$watch('hidden', function (hidden) {
-          if (childElement) {
-            animate.leave(childElement);
-            childElement = null;
-          }
-
-          if (childScope) {
-            childScope.$destroy();
-            childScope = null;
-          }
-
-          if (!hidden) {
-            childScope = $scope.$new();
-            transclude(childScope, function (clone) {
-              childElement = clone;
-              animate.enter(childElement, $element, $element.find('.blockquote-target'));
-            });
-          }
-        });
-      };
+    link: function (scope) {
+      scope.hidden = true;
     }
   };
-}]);
+});
