1a50d1258add4e7d2dcc2d2184ecd075754969dc
Laurent Perrin <laurent.perrin@gmail.com>
2013-07-24 15:53:16 +0200

    added basic transclude.

--- 5_blockquote/blockquote.js
+++ 5_blockquote/blockquote.js
@@ -1,4 +1,17 @@
 var demoApp = angular.module('demoApp', []);
 
 demoApp.directive('blockquote', function () {
+  return {
+    replace: true,
+
+    transclude: true,
+
+    restrict: 'E',
+
+    templateUrl: 'blockquote-tpl',
+
+    link: function (scope, element, attrs) {
+      scope.hidden = true;
+    }
+  };
 });
