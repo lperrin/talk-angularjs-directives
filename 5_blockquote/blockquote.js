dd00e31c9d142b4d9b6a2451bb2b1bb13d340911
Laurent Perrin <laurent.perrin@gmail.com>
2013-07-24 15:56:50 +0200

    with manual transclusion.

--- 5_blockquote/blockquote.js
+++ 5_blockquote/blockquote.js
@@ -10,8 +10,32 @@ demoApp.directive('blockquote', function () {
 
     templateUrl: 'blockquote-tpl',
 
-    link: function (scope, element, attrs) {
-      scope.hidden = true;
+    compile: function (element, attrs, transclude) {
+      return function ($scope, $element, $attrs) {
+        var childElement, childScope;
+
+        $scope.hidden = true;
+
+        $scope.$watch('hidden', function (hidden) {
+          if (childElement) {
+            childElement.remove();
+            childElement = null;
+          }
+
+          if (childScope) {
+            childScope.$destroy();
+            childScope = null;
+          }
+
+          if (!hidden) {
+            childScope = $scope.$new();
+            transclude(childScope, function (clone) {
+              childElement = clone;
+              $element.find('.blockquote-target').append(childElement);
+            });
+          }
+        });
+      };
     }
   };
 });
